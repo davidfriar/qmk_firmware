a2c116efb97e0c5aa0149847eaff1fdf9249cb7c
davidfriar <david.friar@gmail.com>
2019-11-13 22:30:36 +0000

    vim arrow keys

--- keyboards/kbdfans/kbd6x/keymaps/davidfriar/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/davidfriar/keymap.c
@@ -20,29 +20,28 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
       KC_LGUI,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, MT(MOD_RGUI, KC_GRV),
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+      LT(1, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TT(1),
       _______, KC_LCTRL, KC_LALT,                   KC_SPACE,                 KC_RALT, KC_RCTRL, _______
       ),
 
   [1] = LAYOUT(
-      _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______ , _______,
-      _______, KC_BRMD, KC_BRMU, _______, _______, _______, _______, _______, _______, _______, KC_UP, KC_PGUP, _______, KC_DEL,
-      _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______,  _______, KC_LEFT,KC_RIGHT  , KC_PGDN, _______,
-      _______, _______, _______, _______, _______, _______, TG(2), _______,  _______, KC_DOWN,   _______, _______, _______,
+      KC_POWER,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______ , _______,
+      _______, KC_BRID, KC_BRIU, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, KC_DEL,
+      KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT  ,_______, _______, _______,
+      _______, _______, _______, _______, _______, _______, TG(2), KC_PGDN,  _______, _______,   _______, _______, _______,
       _______, _______, _______,                   _______,                   _______, _______, _______
       ),
   [2] = LAYOUT(
-      _______, KC_P1,    KC_P2,   KC_P3,   KC_PPLS,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______, _______ , _______,
-      _______, KC_P4,    KC_P5,   KC_P6, KC_PMNS, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, KC_P7,    KC_P8,   KC_P9, KC_PAST, _______, _______, _______,  _______, _______,_______  , _______, _______,
-      _______, KC_PEQL,   KC_P0,   KC_PDOT, KC_PSLS, _______, _______, _______,  _______, _______,   _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,   KC_9,   KC_PPLS,   _______,   _______,   _______,   _______,
+      _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,   KC_6, KC_PMNS, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,   KC_3, KC_PAST, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, KC_PEQL,   KC_0,   KC_DOT, KC_PSLS, _______, _______,
       _______, _______, _______,                   _______,                   _______, _______, _______
       )
 };
 
-// TODO brightness, caps lock, numpad numbers
 
 void matrix_init_user(void) {
 
